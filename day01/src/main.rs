84d63f20e6762ba253762b13aa8a85dd2e446848
Rasmus lind <rasmus1234555@hotmail.dk>
2024-12-02 22:54:04 +0100

    initialize repo

---
+++ day01/src/main.rs
@@ -0,0 +1,8 @@
+use helpers::read_input;
+
+fn main() {
+    let input = read_input(1); // Day 1
+    println!("Input: {}", input);
+
+    // Solve your puzzle here
+}
---
+++ day02/src/main.rs
@@ -0,0 +1,8 @@
+use helpers::read_input;
+
+fn main() {
+    let input = read_input(2); // Day 1
+    println!("Input: {}", input);
+
+    // Solve your puzzle here
+}
