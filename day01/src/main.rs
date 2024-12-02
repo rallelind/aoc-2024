c3561727835ffc596c09fce2736a474a1cf3293c
Rasmus lind <rasmus1234555@hotmail.dk>
2024-12-02 23:19:41 +0100

    day 1 implementation in rust from js

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,8 +1,47 @@
 use helpers::read_input;
 
+fn calculate_distance(input: &str) -> i32 {
+    let mut left = Vec::new();
+    let mut right = Vec::new();
+
+    for row in input.trim().split('\n') {
+        let nums: Vec<i32> = row
+            .trim()
+            .split_whitespace()
+            .map(|n| n.parse().unwrap())
+            .collect();
+
+        left.push(nums[0]);
+        right.push(nums[1]);
+    }
+
+    let mut total = 0;
+
+    left.sort();
+    right.sort();
+
+    for i in 0..left.len() {
+        total += (left[i] - right[i]).abs();
+    }
+
+    total
+}
+
 fn main() {
     let input = read_input(1); // Day 1
-    println!("Input: {}", input);
+    let total_distance = calculate_distance(&input);
+    println!("Distance: {}", total_distance);
+}
 
-    // Solve your puzzle here
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_calculate_distance() {
+        let input = "1 4\n2 3\n5 6";
+        let expected_distance = 5; // Calculated manually: |1-3| + |2-4| = 5
+        let result = calculate_distance(input);
+        assert_eq!(result, expected_distance);
+    }
 }
