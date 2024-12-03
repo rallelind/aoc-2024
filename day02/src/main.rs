04636cbb42cf3691981b7c2506495c8150f9a5b6
Rasmus lind <rasmus1234555@hotmail.dk>
2024-12-03 10:08:09 +0100

    part 1

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,8 +1,3 @@
-use helpers::read_input;
-
 fn main() {
-    let input = read_input(2); // Day 1
-    println!("Input: {}", input);
-
-    // Solve your puzzle here
+    println!("Hello, world!");
 }
---
+++ day03/src/main.rs
@@ -0,0 +1,35 @@
+use helpers::read_input;
+
+fn extract_and_multiply(input: String) -> i32 {
+    // regex to extract the following match mul(5,5)
+    let re = regex::Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    let mut total = 0;
+
+    for cap in re.captures_iter(&input) {
+        let a: i32 = cap[1].parse().unwrap();
+        let b: i32 = cap[2].parse().unwrap();
+        total += a * b;
+    }
+
+    total
+}
+
+fn main() {
+    let input = read_input(3); // Day 3
+    let result = extract_and_multiply(input);
+    println!("Result: {}", result);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_extract_and_multiply() {
+        let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        let expected_result = 2 * 4 + 5 * 5 + 11 * 8 + 8 * 5;
+        let result = extract_and_multiply(input.to_string());
+        assert_eq!(result, expected_result);
+    }
+}
