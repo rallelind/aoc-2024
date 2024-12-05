279aa0cc5ad139d29e07fda60622d121dd104b12
Rasmus lind <rasmus1234555@hotmail.dk>
2024-12-05 22:37:41 +0100

    day 4 p1

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,9 +1,59 @@
-fn count_xmas() -> i32 {
-    0
+use helpers::read_input;
+
+fn count_xmas(input: &str) -> i32 {
+    // each line should be an array of characters
+    let lines: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
+
+    let mut count = 0;
+
+    let directions = [
+        (0, 1),   // right
+        (1, 0),   // down
+        (1, 1),   // diagonal down right
+        (-1, 1),  // diagonal down left
+        (0, -1),  // left
+        (-1, 0),  // up
+        (-1, -1), // diagonal up left
+        (1, -1),  // diagonal up right
+    ];
+
+    // go through rows and columns and check if the word XMAS is present
+    for row in 0..lines.len() {
+        for col in 0..lines[row].len() {
+            for &(row_delta, col_delta) in &directions {
+                let target = ['X', 'M', 'A', 'S'];
+                let mut found = true;
+
+                for (i, &char) in target.iter().enumerate() {
+                    let new_row = row as i32 + row_delta * i as i32;
+                    let new_col = col as i32 + col_delta * i as i32;
+
+                    if new_row < 0
+                        || new_row >= lines.len() as i32
+                        || new_col < 0
+                        || new_col >= lines[row].len() as i32
+                        || lines[new_row as usize][new_col as usize] != char
+                    {
+                        found = false;
+                        break;
+                    }
+                }
+
+                if found {
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    count
 }
 
 fn main() {
-    println!("Hello, world!");
+    let input = read_input(4);
+    let result = count_xmas(&input);
+
+    println!("Result: {}", result);
 }
 
 #[cfg(test)]
@@ -22,8 +72,8 @@ SMSMSASXSS
 SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX";
-        let expected_count = 3;
-        let result = count_xmas();
+        let expected_count = 18;
+        let result = count_xmas(input);
         assert_eq!(result, expected_count);
     }
 }
